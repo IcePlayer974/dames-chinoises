7b8be44506cf62ef91ef7fdf3cf54b5c3acfb69f
P-A01 <pierre.bianchi81@gmail.com>
2022-02-11 09:34:36 +0100

    Update main.c

--- dames chinoises/main.c
+++ dames chinoises/main.c
@@ -1,8 +1,19 @@
-bon b#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
 #include <string.h>
+
+typedef struct{
+    int Coord x;
+    int Coord y;
+    char couleur;
+    int numero;
+
+}pions;
+
+void deplacement_pion()
+
 int main{
 printf("");
 };
-bon bah mtn on peut bosser
+
