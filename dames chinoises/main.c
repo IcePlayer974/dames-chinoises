0f9b1874b733246917c7ea84a0faf3bdbd3278ed
P-A01 <pierre.bianchi81@gmail.com>
2022-02-11 09:50:58 +0100

    Update main.c

--- dames chinoises/main.c
+++ dames chinoises/main.c
@@ -6,12 +6,20 @@
 typedef struct{
     int Coord x;
     int Coord y;
-    char id;
+    char id[2];
 
 }pions;
 
 void deplacement_pion(pion_joueur[]){
+int i;
+char c[2];
 printf("qu'elle pions voulez vous jouer ?");
+fflush(stdin);
+scanf("%s",&c);
+for(i=0;i<9;i++){
+    if(c==pion_joueur)
+}
+
 }
 
 int main{
